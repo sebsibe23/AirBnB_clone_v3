b670b3639ec5c1bbace4321543e795d892fd5745
sebsibe23 <sebsibe1990@proton.me>
2024-02-26 11:38:42 -0800

    q1

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,224 +1,109 @@
 #!/usr/bin/python3
 """
-Handles I/O, writing and reading, of JSON for storage of all class instances
+Contains the FileStorage class
 """
 
 import json
-from models import base_model, amenity, city, place, review, state, user
-from datetime import datetime
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
-strptime = datetime.strptime
-to_json = base_model.BaseModel.to_json
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
-    """
-    Handles long-term storage of all class instances in JSON format.
-    """
+    """serializes instances to a JSON file & deserializes back to instances"""
 
-    CNC = {
-        'BaseModel': base_model.BaseModel,
-        'Amenity': amenity.Amenity,
-        'City': city.City,
-        'Place': place.Place,
-        'Review': review.Review,
-        'State': state.State,
-        'User': user.User
-    }
-
-    """
-    CNC - this variable is a dictionary with:
-    keys: Class Names
-    values: Class type (used for instantiation)
-    """
-
-    __file_path = './dev/file.json'
+    # string - path to the JSON file
+    __file_path = "file.json"
+    # dictionary - empty but will store all objects by <class name>.id
     __objects = {}
 
     def all(self, cls=None):
-        """
-        Returns a dictionary of all objects or objects of a specific class.
-
-        Parameters:
-        - cls (str): The class name (optional).
-
-        Returns:
-        - dict: A dictionary of objects with the format "ClassName.object_id".
-
-        Raises:
-        - None.
-        """
+        """returns the dictionary __objects"""
         if cls is not None:
-            new_objs = {}
-            for clsid, obj in FileStorage.__objects.items():
-                if type(obj).__name__ == cls:
-                    new_objs[clsid] = obj
-            return new_objs
-        else:
-            return FileStorage.__objects
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    new_dict[key] = value
+            return new_dict
+        return self.__objects
 
     def new(self, obj):
-        """
-        Adds a new object to the storage.
-
-        Parameters:
-        - obj (object): The object to be added.
-
-        Returns:
-        - None.
-
-        Raises:
-        - None.
-        """
-        bm_id = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[bm_id] = obj
+        """sets in __objects the obj with key <obj class name>.id"""
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            self.__objects[key] = obj
 
     def save(self):
-        """
-        Serializes the objects to a JSON file.
+        """serializes __objects to the JSON file (path: __file_path)"""
+        json_objects = {}
+        for key in self.__objects:
+            json_objects[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_objects, f)
 
-        Parameters:
-        - None.
-
-        Returns:
-        - None.
-
-        Raises:
-        - Exception: If an error occurs while saving the objects.
-        """
+    """def reload(self):
+        deserializes the JSON file to __objects
         try:
-            fname = FileStorage.__file_path
-            storage_d = {}
-            for bm_id, bm_obj in FileStorage.__objects.items():
-                storage_d[bm_id] = bm_obj.to_json(saving_file_storage=True)
-            with open(fname, mode='w', encoding='utf-8') as f_io:
-                json.dump(storage_d, f_io)
+            with open(self.__file_path, 'r') as f:
+                jo = json.load(f)
+            for key in jo:
+                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+        # except:
         except Exception as e:
-            raise Exception("Failed to save the objects
-                            to the JSON file.") from e
-
+            raise e
+    """        # pass
     def reload(self):
-        """
-        Deserializes the JSON file to objects.
+        from os import path
 
-        Parameters:
-        - None.
-
-        Returns:
-        - None.
-
-        Raises:
-        - None.
-        """
-        fname = FileStorage.__file_path
-        FileStorage.__objects = {}
+        if not path.isfile(self.__file_path):
+            with open(self.__file_path, 'w') as f:
+                f.write("{}")
         try:
-            with open(fname, mode='r', encoding='utf-8') as f_io:
-                new_objs = json.load(f_io)
+            with open(self.__file_path, 'r') as f:
+                data = json.load(f)
         except Exception as e:
-            raise Exception("Failed to delete the object
-                            from the storage.") from e
-            return
-        for o_id, d in new_objs.items():
-            k_cls = d['__class__']
-            FileStorage.__objects[o_id] = FileStorage.CNC[k_cls](**d)
+            raise e
 
     def delete(self, obj=None):
-        """
-        Deletes an object from the storage.
-
-        Parameters:
-        - obj (object): The object to be deleted (optional).
-
-        Returns:
-        - None.
-
-        Raises:
-        - Exception: If an error occurs while deleting the object.
-        """
-        try:
-            if obj:
-                obj_ref = "{}.{}".format(type(obj).__name__, obj.id)
-                all_class_objs = self.all(obj.__class__.__name__)
-                if all_class_objs.get(obj_ref):
-                    del FileStorage.__objects[obj_ref]
-                self.save()
-        except Exception as e:
-            raise Exception("Failed to delete the object
-                            from the storage.") from e
-
-    def delete_all(self):
-        """
-        Deletes all stored objects from the storage.
-
-        Parameters:
-        - None.
-
-        Returns:
-        - None.
-
-        Raises:
-        - Exception: If an error occurs while deleting the objects.
-        """
-        try:
-            with open(FileStorage.__file_path, mode='w') as f_io:
-                pass
-        except Exception as e:
-            raise Exception("Failed to delete all
-                            objects from the storage.") from e
-        del FileStorage.__objects
-        FileStorage.__objects = {}
-        self.save()
+        """delete obj from __objects if it’s inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
 
     def close(self):
-        """
-        Closes the storage and reloads the objects.
-
-        Parameters:
-        - None.
-
-        Returns:
-        - None.
-
-        Raises:
-        - Exception: If an error occurs while closing the storage.
-        """
-        try:
-            self.reload()
-        except Exception as e:
-            raise Exception("Failed to close the storage.")from e
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
 
     def get(self, cls, id):
         """
-        Retrieves an object based on the class name and id.
-
-        Parameters:
-        - cls (str): The class name.
-        - id (str): The id of the object.
-
+        Retrieves an object based on the class and its ID
+        Args:
+        cls (class): The class of the object
+        id (str): The ID of the object
         Returns:
-        - object: The retrieved object or None if not found.
-
-        Raises:
-        - None.
+        The object if found, None otherwise
         """
-        if cls and id:
-            fetch_obj = "{}.{}".format(cls, id)
-            all_obj = self.all(cls)
-            return all_obj.get(fetch_obj)
+        objects = self.all(cls)
+        for obj in objects.values():
+            if obj.id == id:
+                return obj
         return None
 
     def count(self, cls=None):
         """
-        Counts the number of objects in the storage.
-
-        Parameters:
-        - cls (str): The class name (optional).
-
+        Counts the number of objects in storage
+        Args:
+        cls (class, optional): The class to filter the count (default: None)
         Returns:
-        - int: The number of objects.
-
-        Raises:
-        - None.
+        The number of objects in storage
         """
-        return len(self.all(cls))
+        objects = self.all(cls)
+        return len(objects)
--- models/user.py
+++ models/user.py
@@ -2,21 +2,18 @@
 """
 User Class from Models Module
 """
-
 import hashlib
 import os
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, Float
-
 STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class User(BaseModel, Base):
     """
-    User class handles all application users
+        User class handles all application users
     """
-
     if STORAGE_TYPE == "db":
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
@@ -34,38 +31,19 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """
-        Instantiates a User object.
-
-        Parameters:
-            *args: Variable length argument list.
-            **kwargs: Arbitrary keyword arguments.
-
-        Exceptions:
-            None.
+            instantiates user object
         """
-        try:
-            if kwargs:
-                pwd = kwargs.pop('password', None)
-                if pwd:
-                    User.__set_password(self, pwd)
-            super().__init__(*args, **kwargs)
-        except Exception as e:
-            print(f"An error occurred during User initialization: {str(e)}")
+        if kwargs:
+            pwd = kwargs.pop('password', None)
+            if pwd:
+                User.__set_password(self, pwd)
+        super().__init__(*args, **kwargs)
 
     def __set_password(self, pwd):
         """
-        Custom setter: Encrypts the password to MD5.
-
-        Parameters:
-            pwd (str): The password to be encrypted.
-
-        Exceptions:
-            None.
+            custom setter: encrypts password to MD5
         """
-        try:
-            secure = hashlib.md5()
-            secure.update(pwd.encode("utf-8"))
-            secure_password = secure.hexdigest()
-            setattr(self, "password", secure_password)
-        except Exception as e:
-            print(f"An error occurred during password encryption: {str(e)}")
+        secure = hashlib.md5()
+        secure.update(pwd.encode("utf-8"))
+        secure_password = secure.hexdigest()
+        setattr(self, "password", secure_password)
